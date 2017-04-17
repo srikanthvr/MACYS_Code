a8a31a3791746399577026df650e4a91af5490b6
srikanth reddy vangala <srikanthreddyvangala@srikanths-MacBook-Air.local>
2017-04-17 14:45:40 -0400

    Added the changes

--- MeaningTest/ViewController.swift
+++ MeaningTest/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var abbrevationTableVW: UITableView!
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
